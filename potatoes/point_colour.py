539acbe73261ba9daeb08c2755955a674043d744
Sarath <sarath2000is@gmail.com>
2021-11-16 23:25:43 +0530

    fps+distance

--- potatoes/point_colour.py
+++ potatoes/point_colour.py
@@ -2,26 +2,24 @@ import cv2
 import numpy as np
 import math
 
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+target_x=int(cv2.CAP_PROP_FRAME_HEIGHT/2)
+target_y=int(cv2.CAP_PROP_FRAME_WIDTH/2)
+
 def distance(x1, y1, x2, y2):
-    """
-    Calculate distance between two points
-    """
     dist = math.sqrt(math.fabs(x2-x1)**2 + math.fabs(y2-y1)**2)
     return dist
 
-def find_color1(frame):
-    """
-    Filter "frame" for HSV bounds for color1 (inplace, modifies frame) & return coordinates of the object with that color
-    """
+# Blue
+def find_point1(frame):
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    hsv_lowerbound = np.array([0, 132, 101]) #replace THIS LINE w/ your hsv lowerb
-    hsv_upperbound = np.array([179, 227, 255])#replace THIS LINE w/ your hsv upperb
+    hsv_lowerbound = np.array([0, 132, 101])
+    hsv_upperbound = np.array([179, 227, 255])
     mask = cv2.inRange(hsv_frame, hsv_lowerbound, hsv_upperbound)
-    res = cv2.bitwise_and(frame, frame, mask=mask) #filter inplace
+    res = cv2.bitwise_and(frame, frame, mask=mask)
     cnts, hir = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if len(cnts) > 0:
         maxcontour = max(cnts, key=cv2.contourArea)
-
         #Find center of the contour 
         M = cv2.moments(maxcontour)
         if M['m00'] > 0 and cv2.contourArea(maxcontour) > 1000:
@@ -29,14 +27,12 @@ def find_color1(frame):
             cy = int(M['m01']/M['m00'])
             return (cx, cy), True
         else:
-            return (700, 700), False #faraway point
+            return (700, 700), False
     else:
-        return (700, 700), False #faraway point
+        return (700, 700), False
 
-def find_color2(frame):
-    """
-    Filter "frame" for HSV bounds for color1 (inplace, modifies frame) & return coordinates of the object with that color
-    """
+# Red
+def find_point2(frame):    
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     hsv_lowerbound=np.array([0,28,226])
     hsv_upperbound=np.array([66,243,255])
@@ -45,7 +41,6 @@ def find_color2(frame):
     cnts, hir = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if len(cnts) > 0:
         maxcontour = max(cnts, key=cv2.contourArea)
-
         #Find center of the contour 
         M = cv2.moments(maxcontour)
         if M['m00'] > 0 and cv2.contourArea(maxcontour) > 2000:
@@ -53,28 +48,32 @@ def find_color2(frame):
             cy = int(M['m01']/M['m00'])
             return (cx, cy), True #True
         else:
-            return (700, 700), True #faraway point
+            return (700, 700), True
     else:
-        return (700, 700), True #faraway point
+        return (700, 700), True
 
-def dist(x,y,u,v):
-    return str(math.sqrt((u-x)**2+(v-y)**2))
-
-cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+def find_click(event,x,y,float,params):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        return(x,y), True
+    else:
+        return(target_x,target_y),False
+        
 
 while(1):
-    timer=cv2.getTickCount()
     _, orig_frame = cap.read()
-    #we'll be inplace modifying frames, so save a copy
     copy_frame = orig_frame.copy() 
-    (color1_x, color1_y), found_color1 = find_color1(copy_frame)
-    (color2_x, color2_y), found_color2 = find_color2(copy_frame)
+    cv2.imshow('AngleCalc', copy_frame)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+    (color1_x, color1_y), found_point1 = find_point1(copy_frame)
+    # (target_x, target_y) = (0,0)
+    (target_x, target_y)=cv2.setMouseCallback("AngleCalc",find_click)
 
-    #draw circles around these objects
-    cv2.circle(copy_frame, (color1_x, color1_y), 20, (255, 0, 0), -1)
-    cv2.circle(copy_frame, (color2_x, color2_y), 20, (0, 128, 255), -1)
+    #draw circles around the objects
+    cv2.circle(copy_frame, (color1_x, color1_y), 5, (255, 0, 0), -1)
+    cv2.circle(copy_frame, (target_x, target_y), 5, (0, 128, 255), -1)
 
-    if found_color1 and found_color2:
+    if found_point1 and found_point2:
         #trig stuff to get the line
         hypotenuse = distance(color1_x, color1_x, color2_x, color2_y)
         horizontal = distance(color1_x, color1_y, color2_x, color1_y)
@@ -100,19 +99,8 @@ while(1):
         elif color2_y > color1_y and color2_x > color1_x:
             angle_text = str(360 - angle)
         
-        distance_text=""
-        if(angle_text!=np.NaN):
-            distance_text=dist(color1_x,color1_y,color2_x,color2_y)
-
         #CHANGE FONT HERE
-        cv2.putText(copy_frame, "angle="+angle_text, (30,60), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 128, 229), 2)
-        cv2.putText(copy_frame, "distance="+distance_text, (30,90), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 128, 229), 2)
+        cv2.putText(copy_frame, angle_text, (color1_x-30, color1_y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 128, 229), 2)
 
-        fps =str(int(cv2.getTickFrequency()/(cv2.getTickCount()-timer)))
-        cv2.putText(copy_frame,"FPS:"+fps,(30,30),cv2.FONT_HERSHEY_COMPLEX, 1, (0, 128, 229), 2)
-
-    cv2.imshow('AngleCalc', copy_frame)
-    if cv2.waitKey(5) & 0xFF == ord('q'):
-        break
 cap.release()
 cv2.destroyAllWindows()
